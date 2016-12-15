bc355c7ab35d20f9b6eb5c05f7bc9d2a36db5043
Josue Mazco Puma <jossmp@gmail.com>
2016-12-15 06:24:16 -0500

    Initial commit

--- ejemplo.php
+++ ejemplo.php
@@ -3,7 +3,7 @@
     require ("spp.php");
 
     $search = new SPP();
-    $dni="22306219";
+    $dni="00000000";
     header('Content-type: application/json');
     echo json_encode( $search->BuscaDatoSPP($dni), JSON_PRETTY_PRINT );
 ?>
